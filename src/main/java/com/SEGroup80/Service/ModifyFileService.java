2e49bfd2703010a72f87e22f9edd84ff660174bc
wxr <wangxr@bupt.edu.cn>
2021-05-25 20:16:46 +0800

    version-1.0.2

--- src/main/java/com/SEGroup80/Service/ModifyFileService.java
+++ src/main/java/com/SEGroup80/Service/ModifyFileService.java
@@ -1,10 +1,22 @@
 package com.SEGroup80.Service;
 
+import com.SEGroup80.IO.JSONFileProcessor;
+import com.SEGroup80.IO.UserJSONFileProcessor;
+import com.SEGroup80.Pojo.UserPojo.User;
+
+import java.io.IOException;
+
 public class ModifyFileService {
-    //TODO: modify function
-    public void modifyUserFile(String userID){
 
+    private JSONFileProcessor userJSONFileProcessor = new UserJSONFileProcessor();
 
+    public void modifyUserFile(User user) throws IOException {
+        if (user != null) {
+            userJSONFileProcessor.deleteJSON(user.getUserID());
+            userJSONFileProcessor.writeJSON(user, user.getUserID());
+        } else {
+            System.out.println("The user is null so that cannot be updated!");
+        }
     }
 
 
